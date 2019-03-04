a1624ef2f428828c5de91bf68725bd3338acd9db
Andrew Menning <menning23@gmail.com>
2019-03-04 17:00:59 -0500

    Lecture 317 A leaner switch case statement

--- Section16_ReduxAdvanced/src/store/reducers/result.js
+++ Section16_ReduxAdvanced/src/store/reducers/result.js
@@ -5,6 +5,14 @@ const initialState = {
     results: []
 }
 
+const deleteResult = (state, action) => {
+    const updatedArray = state.results.filter(result => result.id !== action.resultElId);
+    return updateObject(
+        state,
+        {results: updatedArray}
+    );
+};
+
 const reducer = (state = initialState, action) => {
     switch (action.type) {
         case actionTypes.STORE_RESULT:
@@ -15,14 +23,7 @@ const reducer = (state = initialState, action) => {
                 }
             );
         case actionTypes.DELETE_RESULT:
-            // const id = 2;
-            // const newArray = [...state.results];
-            // newArray.splice(id, 1);
-            const updatedArray = state.results.filter(result => result.id !== action.resultElId);
-            return updateObject(
-                state,
-                {results: updatedArray}
-            );
+            return deleteResult(state, action);
     }
 
     return state;
